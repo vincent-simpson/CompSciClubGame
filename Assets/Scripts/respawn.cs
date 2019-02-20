866c5552b0fe6452e9d99430a07d5e88daa2d6a0
Vincent Simpson <34716206+vincentsimpson@users.noreply.github.com>
2019-02-20 17:34:18 -0500

    Added respawn mechanic

---
+++ Assets/Scripts/respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+     public float threshold;
+     public Transform playerSpawnPoint;
+     public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    { 
+          if(player.transform.position.y < threshold)
+          {
+               player.transform.position = playerSpawnPoint.position;
+          }
+    }
+}
